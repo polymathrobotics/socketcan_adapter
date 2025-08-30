6495e90851b9bda897f4836b64a857a0cc21268f
Zeerek Ahmad <zeerekahmad@hotmail.com>
2025-08-30 05:41:03 +0000

    Add default filters to keep CAN flowing

--- socketcan_adapter_ros/launch/socketcan_bridge_launch.py
+++ socketcan_adapter_ros/launch/socketcan_bridge_launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
     # Define args
     can_interface_arg = DeclareLaunchArgument('can_interface', default_value='can0')
     can_error_mask_arg = DeclareLaunchArgument('can_error_mask', default_value='0x1FFFFFFF')
-    can_filter_list_arg = DeclareLaunchArgument('can_filter_list', default_value="['']")
+    can_filter_list_arg = DeclareLaunchArgument('can_filter_list', default_value="['0:0']")
     join_filters_arg = DeclareLaunchArgument('join_filters', default_value='false')
     receive_timeout_arg = DeclareLaunchArgument('receive_timeout_s', default_value='1.0')
 
